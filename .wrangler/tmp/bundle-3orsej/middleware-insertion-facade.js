187e5602ac5a463ef77bf8c429330aa734476d51
gomathie <gomathie@byui.edu>
2026-08-04 00:50:46 +0300

    Refactor code structure for improved readability and maintainability

---
+++ .wrangler/tmp/bundle-3orsej/middleware-insertion-facade.js
@@ -0,0 +1,11 @@
+				import worker, * as OTHER_EXPORTS from "C:\\Users\\gomat\\Downloads\\DEV PROJECTS\\dstack\\.wrangler\\tmp\\pages-Jl8B5r\\functionsWorker-0.2978525270327814.mjs";
+				import * as __MIDDLEWARE_0__ from "C:\\Users\\gomat\\AppData\\Local\\npm-cache\\_npx\\c943b712072b77c4\\node_modules\\wrangler\\templates\\middleware\\middleware-ensure-req-body-drained.ts";
+import * as __MIDDLEWARE_1__ from "C:\\Users\\gomat\\AppData\\Local\\npm-cache\\_npx\\c943b712072b77c4\\node_modules\\wrangler\\templates\\middleware\\middleware-miniflare3-json-error.ts";
+
+				export * from "C:\\Users\\gomat\\Downloads\\DEV PROJECTS\\dstack\\.wrangler\\tmp\\pages-Jl8B5r\\functionsWorker-0.2978525270327814.mjs";
+				const MIDDLEWARE_TEST_INJECT = "__INJECT_FOR_TESTING_WRANGLER_MIDDLEWARE__";
+				export const __INTERNAL_WRANGLER_MIDDLEWARE__ = [
+					
+					__MIDDLEWARE_0__.default,__MIDDLEWARE_1__.default
+				]
+				export default worker;
